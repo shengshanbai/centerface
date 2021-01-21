68d4e1777683ef2fe14d4861a1f5a2d9dcc36cd4
shengshanbai <shengshanbai@126.com>
2021-01-21 21:47:05 +0800

    test code

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import torch
 g_config=EasyDict()
 g_config.input_res=800
 g_config.output_res=200
-g_config.device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+g_config.dev43ice=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
--- test.py
+++ test.py
@@ -26,6 +26,72 @@ class ValImageDataset(data.Dataset):
     def __len__(self):
         return self.size
 
+def preprocessImage(image):
+    mean = np.array([0.40789654, 0.44719302, 0.47026115],
+                         dtype=np.float32).reshape(1, 1, 3)
+    std = np.array([0.28863828, 0.27408164, 0.27809835],
+                        dtype=np.float32).reshape(1, 1, 3)
+    height,width=image.shape[0:2]
+    pad_right=(32-width%32)%32
+    pad_bottom=(32-height%32)%32
+    if pad_right!=0 or pad_bottom!=0:
+        image=cv2.copyMakeBorder(image,0,pad_bottom,0,pad_right,cv2.BORDER_CONSTANT)
+    return (image-mean)/std
+
+def nms(bboxes):
+    sorted, indices=torch.sort(bboxes[:,4],descending=True)
+    bboxes=bboxes[indices,:]
+    added_ids=[]
+    merged_boxes=[]
+    box_sizes=(bboxes[:,2]-bboxes[:,0])*(bboxes[:,3]-bboxes[:,1])
+    for i in range(bboxes.shape[0]):
+        if i not in added_ids:
+            cur_box=bboxes[i]
+            added_ids.append(i)
+            merged_boxes.append(cur_box.detach().cpu().numpy())
+            cur_size=box_sizes[i]
+            left_size=box_sizes[i+1:]
+            inner_x0=bboxes[i+1:,0].clone()
+            inner_x0[inner_x0<cur_box[0]]=cur_box[0]
+            inner_y0=bboxes[i+1:,1].clone()
+            inner_y0[inner_y0<cur_box[1]]=cur_box[1]
+            inner_x1=bboxes[i+1:,2].clone()
+            inner_x1[inner_x1>cur_box[2]]=cur_box[2]
+            inner_y1=bboxes[i+1:,3].clone()
+            inner_y1[inner_y1>cur_box[3]]=cur_box[3]
+            inner_sizes=(inner_y1-inner_y0)*(inner_x1-inner_x0)
+            ratio=inner_sizes/(left_size+cur_size-inner_sizes)
+            tomerge_ids=torch.where(ratio>0.3)[0]
+            tomerge_ids+=(i+1)
+            added_ids.extend(tomerge_ids.cpu().tolist())
+    if len(merged_boxes)>0:
+        return np.vstack(merged_boxes)
+    return None
+
+def parseOutput(outs):
+    heat_map, center_off, wh, landmarks=outs
+    pos_index=torch.where(heat_map>0.5)
+    cy_int=pos_index[2]
+    cx_int=pos_index[3]
+    cx_off=center_off[pos_index]
+    cy_off=center_off[pos_index[0],pos_index[1]+1,pos_index[2],pos_index[3]]
+    cx=cx_int+cx_off
+    cy=cy_int+cy_off
+    w=torch.exp(wh[pos_index])
+    h=torch.exp(wh[pos_index[0],pos_index[1]+1,pos_index[2],pos_index[3]])
+    bboxes=torch.zeros((w.shape[0],5))
+    bboxes[:,0]=cx-w/2
+    bboxes[:,1]=cy-h/2
+    bboxes[:,2]=cx+w/2
+    bboxes[:,3]=cy+h/2
+    bboxes[:,4]=heat_map[pos_index]
+    return nms(bboxes)
+
+def limitBoxesRange(bboxes,x_max,y_max):
+    bboxes[:,0:4]*=4
+    bboxes[:,[0,2]]=np.clip(bboxes[:,[0,2]],0,x_max)
+    bboxes[:,[1,3]]=np.clip(bboxes[:,[1,3]],0,y_max)
+    return bboxes
 
 if __name__=='__main__':
     model = models.CenterFaceNet()
@@ -36,3 +102,16 @@ if __name__=='__main__':
     val_db=ValImageDataset(r'C:\Users\sheng\Downloads\data\WIDER\val_lmdb')
     for item in val_db:
         fname,image=item
+        fimage=preprocessImage(image)
+        fimage=np.transpose(fimage,[2,0,1])
+        cuda_image=torch.from_numpy(fimage[np.newaxis,:,:,:]).to(g_config.device)
+        outs=model(cuda_image)
+        bboxes=parseOutput(outs)
+        if bboxes is not None:
+            bboxes=limitBoxesRange(bboxes,image.shape[1]-1,image.shape[0]-1)
+            for i in range(bboxes.shape[0]):
+                box=bboxes[i]
+                cv2.rectangle(image,(box[0],box[1]),(box[2],box[3]),(0,255,255),thickness=1)
+        cv2.imshow("image",image)
+        cv2.waitKey()
+        cv2.destroyWindow("image")
